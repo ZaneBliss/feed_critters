c54eb83295fabce8255da67aa8b1adea4d43dd8b
zanebliss <zanebliss@icloud.com>
2020-07-15 13:11:09 -0500

    .

--- feed_critters.py
+++ feed_critters.py
@@ -57,11 +57,11 @@ class Fish:
         
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
-        
+
     def __str__(self):
         return f"{self.name} is a {self.species}"
 
 jack = Donkey('Jack', 'Irish', 'midday', 'kibles')
 tina = Llama('Tina', 'Llama', 'morning', 'bits')
-bruce = Snake('Bruce', 'Copperhead')
-gigi = Fish('Gigi', 'Clown')
+bruce = Snake('Bruce', 'Copperhead','mice')
+gigi = Fish('Gigi', 'Clown', 'flakes')
